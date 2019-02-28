26333748d01766fd9c9da14aa5efb7de1ec22a9d
holen <hu.l@ikcrm.com>
2019-02-28 18:32:52 +0800

    add slim

--- app/controllers/wx_controller.rb
+++ app/controllers/wx_controller.rb
@@ -1,21 +1,22 @@
 class WxController < ApplicationController
+
 	def index
-		return render text: params[:echostr] if valid!
+		return render text: params[:echostr] if valid?
 		render text: "error"
 	end
 
 	private
 
-	def valid!
+	def valid?
 		return false if params.blank?
 
 		signature = params[:signature]
     timestamp = params[:timestamp]
     nonce = params[:nonce]
     token = 'iholen'
-
     list = [token, timestamp, nonce]
 
 		Digest::SHA1.hexdigest(list.sort.join) == signature
 	end
+
 end
